cc1964e0b05dc2ea79afac805f7ebd8381c90680
llt <418797644@qq.com>
2025-11-21 01:01:36 +0800

    update

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import '@/envConfig'
+
+import type { Config } from 'drizzle-kit'
+
+export default {
+  schema: './lib/db/schema',
+  dialect: 'postgresql',
+  out: './lib/db/migrations',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+} satisfies Config
