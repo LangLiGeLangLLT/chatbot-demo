75242d6f9c3b4aeb4e38829f53e4b65686d02d22
llt <418797644@qq.com>
2025-11-22 00:16:07 +0800

    update

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,12 +1,14 @@
-import { streamText, UIMessage, convertToModelMessages } from 'ai'
-import { createOpenAICompatible } from '@ai-sdk/openai-compatible'
-
-const qwen = createOpenAICompatible({
-  name: 'qwen',
-  apiKey: process.env.DASHSCOPE_API_KEY,
-  baseURL: 'https://dashscope.aliyuncs.com/compatible-mode/v1',
-  includeUsage: true,
-})
+import {
+  streamText,
+  UIMessage,
+  convertToModelMessages,
+  tool,
+  stepCountIs,
+} from 'ai'
+import { qwen } from '@/lib/ai/qwen'
+import z from 'zod'
+import { createResource } from '@/lib/actions/resources'
+import { findRelevantContent } from '@/lib/ai/embedding'
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30
@@ -20,8 +22,33 @@ export async function POST(req: Request) {
   const result = streamText({
     model: qwen('qwen3-max'),
     messages: convertToModelMessages(messages),
-    system:
-      'You are a helpful assistant that can answer questions and help with tasks',
+    system: `You are a helpful assistant. Check your knowledge base before answering any questions.
+    Only respond to questions using information from tool calls.
+    if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
+    stopWhen: stepCountIs(5),
+    tools: {
+      addResource: tool({
+        description: `add a resource to your knowledge base.
+          If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
+        inputSchema: z.object({
+          title: z
+            .string()
+            .describe('the title of the resource to add to the knowledge base'),
+          content: z
+            .string()
+            .describe('the content or resource to add to the knowledge base'),
+        }),
+        execute: async ({ title, content }) =>
+          createResource({ title, content }),
+      }),
+      getInformation: tool({
+        description: `get information from your knowledge base to answer questions.`,
+        inputSchema: z.object({
+          question: z.string().describe('the users question'),
+        }),
+        execute: async ({ question }) => findRelevantContent(question),
+      }),
+    },
   })
   // send sources and reasoning back to the client
   return result.toUIMessageStreamResponse({
---
+++ lib/ai/qwen.ts
@@ -0,0 +1,8 @@
+import { createOpenAICompatible } from '@ai-sdk/openai-compatible'
+
+export const qwen = createOpenAICompatible({
+  name: 'qwen',
+  apiKey: process.env.DASHSCOPE_API_KEY,
+  baseURL: 'https://dashscope.aliyuncs.com/compatible-mode/v1',
+  includeUsage: true,
+})
