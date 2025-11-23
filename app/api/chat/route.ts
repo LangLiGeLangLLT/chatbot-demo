94553970ee06c8783d37d895f7cb5c6fc9bf4b25
llt <418797644@qq.com>
2025-11-23 16:39:56 +0800

    update

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,6 @@
 import { streamText, UIMessage, convertToModelMessages } from 'ai'
 import { qwen } from '@/lib/ai/qwen'
+import prompt from './prompt.md'
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30
@@ -7,13 +8,16 @@ export const maxDuration = 30
 export async function POST(req: Request) {
   const {
     messages,
+    model,
   }: {
     messages: UIMessage[]
+    model: string
+    webSearch: boolean
   } = await req.json()
   const result = streamText({
-    model: qwen('qwen3-omni-flash'),
+    model: qwen(model),
     messages: convertToModelMessages(messages),
-    system: `You are a helpful assistant that can answer questions and help with tasks`,
+    system: prompt,
     tools: {},
   })
   // send sources and reasoning back to the client
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    rules: {
+      '*.md': {
+        loaders: ['raw-loader'],
+        as: '*.js',
+      },
+    },
+  },
   rewrites: async () => {
     return [
       {
