d56d9de85d9947f1fd176c2d76a018f3eed95c15
llt <418797644@qq.com>
2025-11-28 23:21:23 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { Toaster } from '@/components/ui/sonner'
 import { UIText } from '@/types'
+import { AntdRegistry } from '@ant-design/nextjs-registry'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AntdRegistry>{children}</AntdRegistry>
         <Toaster position="top-center" />
       </body>
     </html>
